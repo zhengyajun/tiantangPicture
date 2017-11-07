dc61224643a5cc69a152c31b6e7a6c9c9a3165b3
郑亚军 <zhengyajun_email@163.com>
2017-11-07 21:13:28 +0800

    之前忘记提交setting.py

---
+++ setting.py
@@ -0,0 +1,13 @@
+
+PROXY_POOL_URL = 'http://localhost:5000/get'
+PROXY_IP = None
+COMMON_URL = 'http://www.ivsky.com'     # 共有的url部分  global
+END_PAGE_NUM = 10                   # 要抓取的页数
+STAER_PAGE_NUM = 0
+SWITCH_IP_TIME = 10
+HEADERS = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+}
+COOKIES = {
+    'Cookie' : 'UM_distinctid=15f76bd4f901e6-0014b4468a4cba-3e63430c-100200-15f76bd4f9225e; statistics_clientid=me; BDTUJIAID=02452b44a40a9e21ccb5a0544eccf8c1; arccount43279=c; CNZZDATA87348=cnzz_eid%3D354657808-1509523986-null%26ntime%3D1509605000; CNZZDATA1629164=cnzz_eid%3D512641877-1509524947-null%26ntime%3D1509606068; Hm_lvt_862071acf8e9faf43a13fd4ea795ff8c=1509525639,1509590397; Hm_lpvt_862071acf8e9faf43a13fd4ea795ff8c=1509606732'
+}
